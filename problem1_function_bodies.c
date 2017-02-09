70b69bb4a6cecff175ab36f30ff1ac6b675f13ad
Antoine Dozois <dozois.a@gmail.com>
2017-02-09 15:33:37 +0100

    Initial repo commit

---
+++ problem1_function_bodies.c
@@ -0,0 +1,28 @@
+/*
+##########################################################################################
+Student Functions:
+
+- Implement the bodies of the problem prototypes within this file!
+- The import rl_homework_lib.h is sufficient to solve all the problems.
+
+##########################################################################################
+*/
+
+#include "rl_homework_lib.h"
+
+
+
+// Function of the World
+State           getNextState(State s_t, Action a_t)
+{
+	return s_t;
+}
+float           getNextReward(State s_t)
+{
+        return 0.0;
+}
+int             isTerminalState(State s_t)
+{
+        return 0;
+}
+
